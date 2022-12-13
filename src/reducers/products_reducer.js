0a736ed7be06f9b849b7028d9b087d4cb5998f5b
maxloyko <maxloyko@gmail.com>
2022-12-13 18:18:25 -0500

    add request to fetch products

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,23 +1,30 @@
 import {
-  SIDEBAR_OPEN,
-  SIDEBAR_CLOSE,
-  GET_PRODUCTS_BEGIN,
-  GET_PRODUCTS_SUCCESS,
-  GET_PRODUCTS_ERROR,
-  GET_SINGLE_PRODUCT_BEGIN,
-  GET_SINGLE_PRODUCT_SUCCESS,
-  GET_SINGLE_PRODUCT_ERROR,
+    SIDEBAR_OPEN,
+    SIDEBAR_CLOSE,
+    GET_PRODUCTS_BEGIN,
+    GET_PRODUCTS_SUCCESS,
+    GET_PRODUCTS_ERROR,
+    GET_SINGLE_PRODUCT_BEGIN,
+    GET_SINGLE_PRODUCT_SUCCESS,
+    GET_SINGLE_PRODUCT_ERROR,
 } from '../actions'
 
 const products_reducer = (state, action) => {
-  switch (action.type) {
-    case SIDEBAR_OPEN:
-      return {...state, isSidebarOpen: true}
-    case SIDEBAR_CLOSE:
-      return {...state, isSidebarOpen: false}
-    default:
-      throw new Error(`No Matching "${action.type}" - action type`)
-  }
+    switch (action.type) {
+        case SIDEBAR_OPEN:
+            return {...state, isSidebarOpen: true}
+        case SIDEBAR_CLOSE:
+            return {...state, isSidebarOpen: false}
+        case GET_PRODUCTS_BEGIN:
+            return {...state, products_loading: true}
+        case GET_PRODUCTS_SUCCESS:
+            const featured_products = action.payload.filter((p) => p.featured === true);
+            return {...state, products_loading: false, featured_products, products: action.payload}
+        case GET_PRODUCTS_ERROR:
+            return {...state,products_loading: false, error: true }
+        default:
+            throw new Error(`No Matching "${action.type}" - action type`)
+    }
 }
 
 export default products_reducer
