e037d5df02a75734ce3ce096b681d08a690db5c6
maxloyko <maxloyko@gmail.com>
2022-12-13 10:48:58 -0500

    make sidebar dynamic

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,12 +9,12 @@ import CartButtons from './CartButtons'
 import { useUserContext } from '../context/user_context'
 
 const Sidebar = () => {
-  const isOpen = true;
+  const {isSidebarOpen, closeSidebar} = useProductsContext();
   return <SidebarContainer>
-    <aside className={`${isOpen ? 'sidebar show-sidebar': 'sidebar'}`}>
+    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar': 'sidebar'}`}>
       <div className="sidebar-header">
         <img src={logo} alt="logo" className='logo'/>
-        <button className='close-btn' type='button'><FaTimes/></button>
+        <button className='close-btn' type='button' onClick={closeSidebar}><FaTimes/></button>
       </div>
       <ul className="links">
         {links.map((link) => {
