d52edb559c5c4993fcd325c8276aaacf0ceeed8f
maxloyko <maxloyko@gmail.com>
2022-12-12 22:46:03 -0500

    basic setup of router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Navbar, Sidebar, Footer} from './components'
+import {Home, Products, Error, Checkout, Cart, SingeProduct, Private, About} from './pages'
 
 function App() {
-  return <h4>Store sloth starter</h4>
+    return (
+            <Router>
+                <Navbar/>
+                <Sidebar/>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/about' element={<About/>}/>
+                    <Route path='/cart' element={<Cart/>}/>
+                    <Route path='/products' element={<Products/>}/>
+                    <Route path='/products/:id' element={<SingeProduct/>}/>
+                    <Route path='/checkout' element={<Private><Checkout/></Private>}/>
+                    <Route path='*' element={<Error/>}/>
+                </Routes>
+                <Footer/>
+            </Router>
+    )
 }
 
 export default App
