6c4ec88188ee201685d34a317f0b2bc01abb2f21
maxloyko <maxloyko@gmail.com>
2022-12-13 17:42:33 -0500

    add hero on home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <h4>hero</h4>
+    return <Wrapper className='section-center'>
+        <article className='content'>
+            <h1>
+                design your <br/>
+                comfort zone
+            </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad assumenda at dignissimos earum inventore
+                quam temporibus vero voluptatem? Praesentium, quia.</p>
+            <Link to='/product' className='hero-btn btn'>show now</Link>
+        </article>
+        <article className='img-container'>
+            <img src={heroBcg} alt="table" className='main-img'/>
+            <img src={heroBcg2} alt="working" className='accent-img'/>
+        </article>
+    </Wrapper>
 }
 
 const Wrapper = styled.section`
   min-height: 60vh;
   display: grid;
   place-items: center;
+
   .img-container {
     display: none;
   }
@@ -23,6 +38,7 @@ const Wrapper = styled.section`
     color: var(--clr-grey-5);
     font-size: 1rem;
   }
+
   @media (min-width: 992px) {
     height: calc(100vh - 5rem);
     grid-template-columns: 1fr 1fr;
@@ -30,17 +46,21 @@ const Wrapper = styled.section`
     h1 {
       margin-bottom: 2rem;
     }
+
     p {
       font-size: 1.25rem;
     }
+
     .hero-btn {
       padding: 0.75rem 1.5rem;
       font-size: 1rem;
     }
+
     .img-container {
       display: block;
       position: relative;
     }
+
     .main-img {
       width: 100%;
       height: 550px;
@@ -49,6 +69,7 @@ const Wrapper = styled.section`
       display: block;
       object-fit: cover;
     }
+
     .accent-img {
       position: absolute;
       bottom: 0;
@@ -57,6 +78,7 @@ const Wrapper = styled.section`
       transform: translateX(-50%);
       border-radius: var(--radius);
     }
+
     .img-container::before {
       content: '';
       position: absolute;
