90cf800951c72726a857db8bcd3eea5f80fd7357
maxloyko <maxloyko@gmail.com>
2022-12-13 17:32:04 -0500

    add error checkout and about pages (styles)

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
-import { PageHero } from '../components'
+import {PageHero} from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+    return <main>
+        <PageHero title="about"/>
+        <Wrapper className='page section section-center'>
+            <img src={aboutImg} alt='about'/>
+            <article>
+                <div className="title">
+                    <h2>About page</h2>
+                    <div className="underline">
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, molestias, mollitia. Culpa ipsa,
+                    quo. A ducimus possimus provident rerum tempore. Dolor dolore expedita iste maiores molestias nobis
+                    reprehenderit repudiandae, voluptate?</p>
+            </article>
+        </Wrapper>
+    </main>
 }
 
 const Wrapper = styled.section`
   display: grid;
   gap: 4rem;
+
   img {
     width: 100%;
     display: block;
@@ -17,6 +33,7 @@ const Wrapper = styled.section`
     height: 500px;
     object-fit: cover;
   }
+
   p {
     line-height: 2;
     max-width: 45em;
@@ -24,12 +41,15 @@ const Wrapper = styled.section`
     margin-top: 2rem;
     color: var(--clr-grey-5);
   }
+
   .title {
     text-align: left;
   }
+
   .underline {
     margin-left: 0;
   }
+
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
