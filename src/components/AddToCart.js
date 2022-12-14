6916865f8fe5438e50bc23201c98bd0324c24544
maxloyko <maxloyko@gmail.com>
2022-12-14 11:15:05 -0500

    add color and amount buttons

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,29 +1,74 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import { FaCheck } from 'react-icons/fa'
-import { useCartContext } from '../context/cart_context'
+import {Link} from 'react-router-dom'
+import {FaCheck} from 'react-icons/fa'
+import {useCartContext} from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>
+const AddToCart = ({product}) => {
+    const {id, stock, colors} = product;
+    const [mainColor, setMainColor] = useState(colors[0])
+    const [amount, setAmount] = useState(1)
+    const increase  = () => {
+        if (amount < stock) {
+            setAmount((prevState) => {
+                return prevState + 1;
+            })
+        }
+    }
+    const decrease = () => {
+        if (amount > 1) {
+            setAmount((prevState) => {
+                return prevState - 1;
+            })
+        }
+    }
+    return <Wrapper>
+        <div className="colors">
+            <span>
+                colors:
+            </span>
+            <div>
+                {
+                    colors.map((color, index) => {
+                       return <button
+                           key={index}
+                           style={{background:color}}
+                           className={`${mainColor === color ? 'color-btn active': 'color-btn'}`}
+                           onClick={() => setMainColor(color)}
+                       >
+                           {mainColor === color ? <FaCheck/>: ''}
+                       </button>
+                    })
+                }
+            </div>
+        </div>
+        <div className="btn-container">
+            <AmountButtons amount={amount} increase={increase} decrease={decrease}/>
+            <Link to={'/cart'} className='btn'>add to cart</Link>
+        </div>
+    </Wrapper>
 }
 
 const Wrapper = styled.section`
   margin-top: 2rem;
+
   .colors {
     display: grid;
     grid-template-columns: 125px 1fr;
     align-items: center;
     margin-bottom: 1rem;
+
     span {
       text-transform: capitalize;
       font-weight: 700;
     }
+
     div {
       display: flex;
     }
   }
+
   .color-btn {
     display: inline-block;
     width: 1.5rem;
@@ -37,14 +82,17 @@ const Wrapper = styled.section`
     display: flex;
     align-items: center;
     justify-content: center;
+
     svg {
       font-size: 0.75rem;
       color: var(--clr-white);
     }
   }
+
   .active {
     opacity: 1;
   }
+
   .btn-container {
     margin-top: 2rem;
   }
