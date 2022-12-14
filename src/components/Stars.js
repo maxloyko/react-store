7952a21806fd81bb41636887d781c65462f9fe5c
maxloyko <maxloyko@gmail.com>
2022-12-14 10:46:25 -0500

    add stars reviews

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,22 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
+import {BsStarFill, BsStarHalf, BsStar} from 'react-icons/bs'
+
+const Stars = ({stars, reviews}) => {
+    return <Wrapper>
+        <div className="stars">
+            <span>
+                {Array.from({length: 5}).map((_, index) => {
+                    let current = index + 1;
+                    if (stars > current) {
+                        return <BsStarFill key={index}/>
+                    } else if (stars > current -1 + 0.5 && stars < current ) {
+                        return <BsStarHalf key={index}/>
+                    } else {
+                        return <BsStar key={index}/>
+                    }
+                })}
+            </span>
+        </div>
+        <div className="reviews">
+            ({reviews}) customer reviews
+        </div>
+    </Wrapper>
 }
 
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
+
   span {
     color: #ffb900;
     font-size: 1rem;
     margin-right: 0.25rem;
   }
+
   p {
     margin-left: 0.5rem;
     margin-bottom: 0;
   }
+
   margin-bottom: 0.5rem;
 `
 export default Stars
--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -42,7 +42,7 @@ const SingleProductPage = () => {
     if (error) {
         return <Error/>
     }
-    const {name, price, description, stock, starts, reviews, id:sku, company, images} = product;
+    const {name, price, description, stock, stars, reviews, id:sku, company, images} = product;
 
     return <Wrapper>
         <PageHero title={name} product/>
@@ -52,7 +52,7 @@ const SingleProductPage = () => {
                 <ProductImages images={images}/>
                 <section className="content">
                     <h2>{name}</h2>
-                    <Stars/>
+                    <Stars stars={stars} reviews={reviews}/>
                     <h5 className='price'>{formatPrice(price)}</h5>
                     <p className="desc">{description}</p>
                     <p className="info">
