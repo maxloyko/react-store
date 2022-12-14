6d53255e53858e742526b13be8541ffe39913ad4
maxloyko <maxloyko@gmail.com>
2022-12-13 21:59:46 -0500

    add format price helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,7 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+    return Intl.NumberFormat('en-Us', {
+        style: 'currency', currency: 'USD'
+    }).format(number / 100)
+}
 
 export const getUniqueValues = () => {}
