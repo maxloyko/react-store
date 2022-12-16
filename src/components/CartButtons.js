cdd71e563ed18991bf8563545ff13e02b123f460
Max Loyko <maxloyko@gmail.com>
2022-12-16 17:20:12 -0500

    add auth0 with user context

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -1,27 +1,35 @@
 import React from 'react'
-import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import {FaShoppingCart, FaUserMinus, FaUserPlus} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
 import styled from 'styled-components'
-import { useProductsContext } from '../context/products_context'
-import { useCartContext } from '../context/cart_context'
-import { useUserContext } from '../context/user_context'
+import {useProductsContext} from '../context/products_context'
+import {useCartContext} from '../context/cart_context'
+import {useUserContext} from '../context/user_context'
 
 const CartButtons = () => {
-  const {closeSidebar} = useProductsContext();
-  const {total_items} = useCartContext()
-  return <Wrapper className='cart-btn-wrapper'>
-    <Link to='/cart' className='cart-btn' onClick={closeSidebar}>
-      Cart
-      <span className='cart-container'>
+    const {loginWithRedirect, myUser, logout} = useUserContext();
+    const {closeSidebar} = useProductsContext();
+    const {total_items} = useCartContext()
+    return <Wrapper className='cart-btn-wrapper'>
+        <Link to='/cart' className='cart-btn' onClick={closeSidebar}>
+            Cart
+            <span className='cart-container'>
         <FaShoppingCart/>
         <span className='cart-value'>{total_items}</span>
       </span>
-    </Link>
-    <button type='button' className='auth-btn'>
-      Login
-      <FaUserPlus/>
-    </button>
-  </Wrapper>
+        </Link>
+        {!myUser ?
+        <button type='button' className='auth-btn' onClick={loginWithRedirect}>
+            Login
+            <FaUserPlus/>
+        </button>
+            :
+        <button type='button' className='auth-btn' onClick={logout}>
+            Logout
+            <FaUserMinus/>
+        </button>
+        }
+    </Wrapper>
 }
 
 const Wrapper = styled.div`
@@ -39,15 +47,18 @@ const Wrapper = styled.div`
 
     align-items: center;
   }
+
   .cart-container {
     display: flex;
     align-items: center;
     position: relative;
+
     svg {
       height: 1.6rem;
       margin-left: 5px;
     }
   }
+
   .cart-value {
     position: absolute;
     top: -10px;
@@ -63,6 +74,7 @@ const Wrapper = styled.div`
     color: var(--clr-white);
     padding: 12px;
   }
+
   .auth-btn {
     display: flex;
     align-items: center;
@@ -72,6 +84,7 @@ const Wrapper = styled.div`
     cursor: pointer;
     color: var(--clr-grey-1);
     letter-spacing: var(--spacing);
+
     svg {
       margin-left: 5px;
     }
